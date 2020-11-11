5d1d37b684b430f4d26a98729bc4d07922cc9a87
jramirez22 <69657150+jramirez22@users.noreply.github.com>
2020-11-10 18:56:55 -0600

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -17,20 +17,31 @@ class Analysis(decafVisitor):
     def __init__(self):
         self.count = 0
         self.currentScope = None
+        self.currentMethod = None
         self.isError = False
         self.scopes = dict()
+        # Using in the location node to find members of a nested struct.
+        self.structs = dict()
         # Manage available data types
         self.T = symtab.Types()
 
     def createLocalScope(self, name):
         name = name + str(self.count)
         self.count += 1
+
         # Create and set as current scope
         localScope = symtab.Scope(name, self.currentScope)
         self.currentScope = localScope
 
     def exitScope(self):
         self.currentScope = self.currentScope.enclosingScope
+
+    def addStruct(self, line, typeName):
+        if self.T.typeSize.get(typeName) is not None:
+            ErrorHandler.structError(line, typeName)
+            self.error()
+
+        self.T.typeSize[typeName] = 0
 
     def createType(self, line, typeName):
         type = symtab.Type(typeName, self.T)
@@ -39,13 +50,6 @@ class Analysis(decafVisitor):
             self.error()
 
         return type
-
-    def addStruct(self, line, typeName):
-        if self.T.types.get(typeName) is not None:
-            ErrorHandler.structError(line, typeName)
-            self.error()
-
-        self.T.types[typeName] = 0
 
     def defineSymbol(self, line, scope, symbol):
         if not scope.define(symbol):
@@ -68,14 +72,6 @@ class Analysis(decafVisitor):
             ErrorHandler.nameError(line, "main()")
             self.error()
 
-    def intChecker(self, line, leftType, rightType, op):
-        if leftType != _int or rightType != _int:
-            ErrorHandler.typeError(line, op)
-            self.error()
-            return _error
-
-        return _int
-
     def condChecker(self, line, stmt, expr):
         if expr != _boolean:
             ErrorHandler.typeError(line, stmt, 2)
@@ -83,6 +79,14 @@ class Analysis(decafVisitor):
             return _error
 
         return _boolean
+
+    def intChecker(self, line, leftType, rightType, op):
+        if leftType != _int or rightType != _int:
+            ErrorHandler.typeError(line, op)
+            self.error()
+            return _error
+
+        return _int
 
     def error(self):
         self.isError = True
@@ -97,37 +101,68 @@ class Analysis(decafVisitor):
         # Check if there is a main method
         self.mainCheker(ctx.start.line, self.currentScope)
 
-    def visitMethodCall(self, ctx: decafParser.MethodCallContext):
+    # Declaration
+    def visitStructDecl(self, ctx: decafParser.StructDeclContext):
+        name = ctx.ID().getText()
+        # Add struct as new type
+        self.addStruct(ctx, name)
+
+        structSymbol = symtab.StructSymbol(name, None, self.currentScope)
+
+        # Define the struct in the current scope
+        self.defineSymbol(ctx.start.line, self.currentScope, structSymbol)
+        # Set struct scope as current scope
+        self.currentScope = structSymbol
+        # Visit block
+        self.visit(ctx.bl)
+        # Save scope
+        self.scopes[ctx] = self.currentScope
+        # Save struct
+        self.structs[name] = self.currentScope
+        # Exit scope
+        self.exitScope()
+
+        # Returns the name to perform the following feature:
+        # struct name {varDecl} var ;
+        return name
+
+    def visitVarDecl(self, ctx: decafParser.VarDeclContext):
+        varType = self.visit(ctx.varType())
         name = ctx.ID().getText()
 
-        methodSymbol = self.currentScope.resolve(name)
+        type = self.createType(ctx.start.line, varType)
+        symbol = symtab.VariableSymbol(name, type)
 
-        # Check that the symbol is defined
-        if not methodSymbol:
-            ErrorHandler.nameError(ctx.start.line, name)
+        self.defineSymbol(ctx.start.line, self.currentScope, symbol)
+
+    def visitArrayDecl(self, ctx: decafParser.ArrayDeclContext):
+        varType = self.visit(ctx.varType())
+        name = ctx.ID().getText()
+
+        num = ctx.NUM().getText()
+        # <NUM> in the declaration of an array must be greater than 0.
+        if int(num) <= 0:
+            ErrorHandler.arrayError(ctx.start.line, name)
             self.error()
 
-        # Check that the symbol is a method
-        isMethod = isinstance(methodSymbol, symtab.MethodSymbol)
-        if not isMethod:
-            ErrorHandler.typeError(ctx.start.line, name, 3)
-            self.error()
+        type = self.createType(ctx.start.line, varType)
+        symbol = symtab.ArraySymbol(name, type, num)
 
-        # Check method signature
-        signature = methodSymbol.args
-        args = []
+        self.defineSymbol(ctx.start.line, self.currentScope, symbol)
 
-        for arg in ctx.arg():
-            type = self.visit(arg)
-            args.append(type)
+    # VarTypes
+    def visitIntType(self, ctx: decafParser.IntTypeContext):
+        return _int
 
-        if not (signature == args):
-            ErrorHandler.typeError(ctx.start.line, name, 4)
-            self.error()
+    def visitCharType(self, ctx: decafParser.CharTypeContext):
+        return _char
 
-        # Return the type of the method
-        # to be able to make methods calls from conditional statements
-        return methodSymbol.type.name
+    def visitBoolType(self, ctx: decafParser.BoolTypeContext):
+        return _boolean
+
+    def visitStructType(self, ctx: decafParser.StructTypeContext):
+        structName = ctx.ID().getText()
+        return structName
 
     def visitMethodDecl(self, ctx: decafParser.MethodDeclContext):
         type = ctx.methodType().getText()
@@ -138,7 +173,7 @@ class Analysis(decafVisitor):
         methodScope = symtab.MethodSymbol(name, type, self.currentScope)
         # Visit the parameters
         for param in ctx.parameter():
-            symbol = self.visitParameter(param)
+            symbol = self.visit(param)
             # Create the signature
             methodScope.args.append(symbol.type.name)
 
@@ -151,6 +186,7 @@ class Analysis(decafVisitor):
         self.defineSymbol(ctx.start.line, self.currentScope, methodScope)
         # Set method scope as current scope
         self.currentScope = methodScope
+        self.currentMethod = methodScope
         # Visit block
         self.visit(ctx.bl)
         # Save scope
@@ -158,28 +194,21 @@ class Analysis(decafVisitor):
         # Exit scope
         self.exitScope()
 
-    def visitParameter(self, ctx: decafParser.ParameterContext):
+    def visitVarParam(self, ctx: decafParser.VarParamContext):
         type = ctx.paramType.getText()
         name = ctx.ID().getText()
         # Create type for the parameter
         type = self.createType(ctx.start.line, type)
+        return symtab.VariableSymbol(name, type)
 
-        return symtab.Symbol(name, type)
+    def visitArrayParam(self, ctx: decafParser.ArrayParamContext):
+        type = ctx.paramType.getText()
+        name = ctx.ID().getText()
+        # Create type for the parameter
+        type = self.createType(ctx.start.line, type)
+        return symtab.ArraySymbol(name, type, None)
 
-    def visitReturnStmt(self, ctx: decafParser.ReturnStmtContext):
-        methodType = self.currentScope.type.name
-        returnType = None
-
-        if ctx.expression() is None:
-            returnType = _void
-        else:
-            returnType = self.visit(ctx.expression())
-
-        # Check that the return type is the same as the method type
-        if returnType != methodType:
-            ErrorHandler.returnError(ctx.start.line, methodType, returnType)
-            self.error()
-
+    # Statement
     def visitIfStmt(self, ctx: decafParser.IfStmtContext):
         stmt = "ifScope"
         expr = self.visit(ctx.expression())
@@ -204,73 +233,61 @@ class Analysis(decafVisitor):
         self.scopes[ctx] = self.currentScope
         self.exitScope()
 
-    def visitStructDecl(self, ctx: decafParser.StructDeclContext):
-        name = ctx.ID().getText()
-        # Add struct as new type
-        self.addStruct(ctx, name)
+    def visitReturnStmt(self, ctx: decafParser.ReturnStmtContext):
+        methodType = self.currentMethod.type.name
+        returnType = None
 
-        structSymbol = symtab.StructSymbol(name, None, self.currentScope)
+        if ctx.expression() is None:
+            returnType = _void
+        else:
+            returnType = self.visit(ctx.expression())
 
-        # Define the struct in the current scope
-        self.defineSymbol(ctx.start.line, self.currentScope, structSymbol)
-        # Set struct scope as current scope
-        self.currentScope = structSymbol
-        # Visit childs
-        self.visit(ctx.bl)
-        # Save scope
-        self.scopes[ctx] = self.currentScope
-        # Exit scope
-        self.exitScope()
-
-        # Returns the name to perform the following feature:
-        # struct name {varDecl} var ;
-        return name
-
-    def visitVarDecl(self, ctx: decafParser.VarDeclContext):
-        varType = self.visit(ctx.varType())
-        name = ctx.ID().getText()
-
-        type = self.createType(ctx.start.line, varType)
-        symbol = symtab.VariableSymbol(name, type)
-
-        self.defineSymbol(ctx.start.line, self.currentScope, symbol)
-
-    def visitArrayDecl(self, ctx: decafParser.ArrayDeclContext):
-        varType = ctx.varType().getText()
-        name = ctx.ID().getText()
-
-        num = ctx.NUM().getText()
-        # <NUM> in the declaration of an array must be greater than 0.
-        if int(num) <= 0:
-            ErrorHandler.arrayError(ctx.start.line, name)
+        # Check that the return type is the same as the method type
+        if returnType != methodType:
+            ErrorHandler.returnError(ctx.start.line, methodType, returnType)
             self.error()
 
-        type = self.createType(ctx.start.line, varType)
-        symbol = symtab.ArraySymbol(name, type, num)
+    def visitMethodCall(self, ctx: decafParser.MethodCallContext):
+        name = ctx.ID().getText()
 
-        self.defineSymbol(ctx.start.line, self.currentScope, symbol)
+        methodSymbol = self.currentScope.resolve(name)
 
-    def visitIntType(self, ctx: decafParser.IntTypeContext):
-        return _int
+        # Check that the symbol is defined
+        if not methodSymbol:
+            ErrorHandler.nameError(ctx.start.line, name)
+            self.error()
 
-    def visitCharType(self, ctx: decafParser.CharTypeContext):
-        return _char
+        # Check that the symbol is a method
+        isMethod = isinstance(methodSymbol, symtab.MethodSymbol)
+        if not isMethod:
+            ErrorHandler.typeError(ctx.start.line, name, 3)
+            self.error()
 
-    def visitBoolType(self, ctx: decafParser.BoolTypeContext):
-        return _boolean
+        # Check method signature
+        signature = methodSymbol.args
+        args = list()
 
-    def visitStructType(self, ctx: decafParser.StructTypeContext):
-        structName = ctx.ID().getText()
-        return structName
+        for arg in ctx.arg():
+            type = self.visit(arg)
+            args.append(type)
+
+        if not (signature == args):
+            ErrorHandler.typeError(ctx.start.line, name, 4)
+            self.error()
+
+        # Return the type of the method
+        # to be able to make methods calls from conditional statements
+        return methodSymbol.type.name
 
     def visitAsignStmt(self, ctx: decafParser.AsignStmtContext):
-        left = self.visit(ctx.location())
-        right = self.visit(ctx.expression())
+        leftType = self.visit(ctx.location())
+        rightType = self.visit(ctx.expression())
 
-        if not left == right:
+        if not leftType == rightType:
             ErrorHandler.typeError(ctx.start.line, "=")
             self.error()
 
+    # Expressions
     def visitLocation(self, ctx: decafParser.LocationContext):
         name = ctx.ID().getText()
         symbol = self.currentScope.resolve(name)
@@ -278,38 +295,54 @@ class Analysis(decafVisitor):
         if not symbol:
             ErrorHandler.nameError(ctx.start.line, name)
             self.error()
+            return _error
 
-        expr = None
-        loc = None
+        # Check if it is a built-in type
+        builtIn = False
+        if symbol.type.name in self.T.builtIn:
+            builtIn = True
 
-        if ctx.expr is not None:
-            expr = self.visit(ctx.expr)
+        if builtIn:
+            # Declared as a variable
+            if isinstance(symbol, symtab.VariableSymbol):
+                if ctx.expr is not None:
+                    ErrorHandler.typeError(ctx.start.line, symbol.name, 5)
+                    self.error()
 
-        if ctx.loc is not None:
-            loc = self.visit(ctx.loc)
+                if ctx.loc is not None:
+                    ErrorHandler.attributeError(ctx.start.line, symbol.name, ctx.loc.getText())
+                    self.error()
 
-        # Declared as a variable
-        if isinstance(symbol, symtab.VariableSymbol):
-            pass
+                return symbol.type.name
 
-        # Declared as an arrangement
-        elif isinstance(symbol, symtab.ArraySymbol):
-            pass
+            # Declared as an array
+            elif isinstance(symbol, symtab.ArraySymbol):
+                try:
+                    index = int(ctx.expr.getText())
+                except ValueError:
+                    index = None
 
-        # Declared as an struct
-        elif isinstance(symbol, symtab.StructSymbol):
-            pass
-        # if not symbol:
-        #     ErrorHandler.nameError(ctx.start.line, name)
-        #     self.error()
+                if (index is None) or (index < 0) or (index >= int(symbol.num)):
+                    ErrorHandler.arrayError(ctx.start.line, symbol.name, 2)
+                    self.error()
 
-        # expr = ctx.expr
+                if ctx.loc is not None:
+                    ErrorHandler.attributeError(ctx.start.line, symbol.name, ctx.loc.getText())
+                    self.error()
 
-        # if expr is None:
+                return symbol.type.name
 
-        # loc = ctx.loc
-
-        # return self.visitChildren(ctx)
+        if not builtIn:
+            # Save the actual scope
+            actualScope = self.currentScope
+            # Because it has a member, we declare the struct as the current scope
+            self.currentScope = self.structs.get(symbol.type.name)
+            # Visit location
+            type = self.visit(ctx.loc)
+            # Restore actual scope
+            self.currentScope = actualScope
+            # Return the type of the location
+            return type
 
     def visitMulExp(self, ctx: decafParser.MulExpContext):
         op = ctx.op.getText()
@@ -333,7 +366,12 @@ class Analysis(decafVisitor):
         leftType = self.visit(ctx.left)
         rightType = self.visit(ctx.right)
 
-        return self.intChecker(ctx.start.line, leftType, rightType, op)
+        if leftType != _int or rightType != _int:
+            ErrorHandler.typeError(ctx.line.start, op)
+            self.error()
+            return _error
+
+        return _boolean
 
     def visitEqExp(self, ctx: decafParser.EqExpContext):
         op = ctx.op.getText()
@@ -352,7 +390,7 @@ class Analysis(decafVisitor):
             self.error()
             return _error
 
-        return leftType
+        return _boolean
 
     def visitCondExp(self, ctx: decafParser.CondExpContext):
         op = ctx.op.getText()
@@ -361,7 +399,7 @@ class Analysis(decafVisitor):
         rightType = self.visit(ctx.right)
 
         if leftType != _boolean or rightType != _boolean:
-            ErrorHandler.typeError(ctx.line.start, op)
+            ErrorHandler.typeError(ctx.start.line, op)
             self.error()
             return _error
 
@@ -387,6 +425,7 @@ class Analysis(decafVisitor):
 
         return _boolean
 
+    # Literals
     def visitIntLiteral(self, ctx: decafParser.IntLiteralContext):
         return _int
 
